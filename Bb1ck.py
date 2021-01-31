e93277aa6a62b1061cdaf33acb493c9f8e60f2e8
BL4cK-11st <78300913+BL4cK-11st@users.noreply.github.com>
2021-01-31 13:11:10 +0100

    Create Bb1ck.py

---
+++ Bb1ck.py
@@ -0,0 +1,22 @@
+# Bb1ck
+
+def morning():
+     print("Bonjour ! ")
+     print(f"""\ Explainations:
+At {time} in france and all country that they talk in french,\n the greet expression is:\n Bonjour that mean Good morning """)
+
+
+
+def evening():
+      print("Bonsoir !")
+      print(f"""\ Explainations:
+At {time} in france and all country that they talk in french,\n the greet expression is:\n Bonsoir that mean Good afternoon or Good evening """)
+
+
+time = int(input("Please enter the time of own to how may you greet in french at these time: "))
+if time <=12:
+   morning()
+elif time >= 12:
+   evening()
+else:
+   pass
